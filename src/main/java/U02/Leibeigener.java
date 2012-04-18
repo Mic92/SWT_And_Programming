7b0fb3e575adacc735452cdc0ce255dd5bd6c358
Jörg Thalheim <jthalheim@gmail.com>
2012-04-18 17:11:05 +0200

    fix negative taxes with "Leibeigener"

--- src/main/java/U02/Leibeigener.java
+++ src/main/java/U02/Leibeigener.java
@@ -2,6 +2,8 @@
 public class Leibeigener extends Bauer {
        @Override
        int zuVersteuerndesEinkommen() {
-               return super.zuVersteuerndesEinkommen() - 12;
+               int steuer = super.zuVersteuerndesEinkommen() - 12;
+               if (steuer < 0) return 0;
+               return steuer;
        }
 }
